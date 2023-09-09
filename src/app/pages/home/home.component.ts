e83131354a09c687261bfe8cba1d61364829bc1d
erikbernard <erikbrsilva@gmail.com>
2023-09-09 09:45:01 -0400

    update: call mock data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -38,7 +38,6 @@ export class HomeComponent implements OnInit {
 			},
 			error: error => {
 				console.error('There was an error!', error);
-			}, complete: () => {
 				this.listNewGames = this.mockData.games;
 			},
 		})
@@ -50,7 +49,6 @@ export class HomeComponent implements OnInit {
 			},
 			error: error => {
 				console.error('There was an error!', error);
-			}, complete: () => {
 				this.listPreSale = this.mockData.games;
 			},
 		})
@@ -62,7 +60,6 @@ export class HomeComponent implements OnInit {
 			},
 			error: error => {
 				console.error('There was an error!', error);
-			}, complete: () => {
 				this.listDemos = this.mockData.games;
 			},
 		})
